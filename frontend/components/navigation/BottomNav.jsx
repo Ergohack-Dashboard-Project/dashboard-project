18390bf4d43dea8b09cdb751bf27fd9f139fbe86
eric c <ecam900@gmail.com>
2021-10-06 01:06:09 -0400

    Add bottom nav state and labels.

--- frontend/components/navigation/BottomNav.jsx
+++ frontend/components/navigation/BottomNav.jsx
@@ -7,33 +7,38 @@ import {
   MenuOutlined,
 } from '@mui/icons-material';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import React from 'react';
+import { useState } from 'react';
 
 // TODO: Change these icons for more appropriate ones if available
 const navLinks = [
   {
     label: 'Token',
     link: '/token-info',
+    value: 'info',
     icon: <FiberSmartRecordOutlined />,
   },
   {
     label: 'Staking',
     link: '/staking',
+    value: 'staking',
     icon: <AutoGraphOutlined />,
   },
   {
     label: 'Home',
     link: '/',
+    value: '/',
     icon: <HomeOutlined />,
   },
   {
     label: 'Dashboard',
     link: '/dashboard',
+    value: 'dashboard',
     icon: <DashboardOutlined />,
   },
   {
     label: 'More',
     link: 'sidemenu',
+    value: 'sidemenu',
     icon: <MenuOutlined />,
   },
 ];
@@ -43,10 +48,20 @@ const root = { position: 'fixed', bottom: -1, width: '100vw' };
 
 const BottomNav = () => {
   const [value, setValue] = useState('/');
+
+  const handleChange = (_, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <BottomNavigation sx={root}>
+    <BottomNavigation showLabels value={value} sx={root} onChange={handleChange}>
       {navLinks.map((link, i) => (
-        <BottomNavigationAction key={`${i}_${link.label}`} label={link.label} value={link.value} icon={link.icon} />
+        <BottomNavigationAction
+          key={`${i}_${link.label}`}
+          label={link.label}
+          value={link.value}
+          icon={link.icon}
+        />
       ))}
     </BottomNavigation>
   );
