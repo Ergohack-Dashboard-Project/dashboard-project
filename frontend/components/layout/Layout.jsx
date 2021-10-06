cf3f91d6aaf80df40eff2e55a53413c582210f70
eric c <ecam900@gmail.com>
2021-10-06 01:20:32 -0400

    Add full page transitions on route change.

--- frontend/components/layout/Layout.jsx
+++ frontend/components/layout/Layout.jsx
@@ -2,7 +2,10 @@ import Header from '@components/Header';
 import BottomNav from '@components/navigation/BottomNav';
 import { useMediaQuery } from '@mui/material';
 import { Box, styled, useTheme } from '@mui/system';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 import makeStyledScrollbar from 'styles/makeStyledScrollbar';
+import pageTransitions from './pageTransitions';
 
 const Root = styled('div')(({ theme }) => ({
   height: '100vh',
@@ -21,11 +24,25 @@ const childrenWrapper = {
 
 const Layout = ({ children }) => {
   const theme = useTheme();
+  const router = useRouter();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   return (
     <Root>
       <Header />
-      <Box sx={childrenWrapper}>{children}</Box>
+      <AnimatePresence exitBeforeEnter>
+        {/* Page transition animations */}
+        <Box
+          component={motion.div}
+          key={router.route}
+          variants={pageTransitions}
+          initial='hidden'
+          animate='visible'
+          exit='hidden'
+          sx={childrenWrapper}
+        >
+          {children}
+        </Box>
+      </AnimatePresence>
       {isMobile && <BottomNav />}
     </Root>
   );
--- frontend/components/navigation/BottomNav.jsx
+++ frontend/components/navigation/BottomNav.jsx
@@ -7,6 +7,7 @@ import {
   MenuOutlined,
 } from '@mui/icons-material';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import router, { useRouter } from 'next/router';
 import { useState } from 'react';
 
 // TODO: Change these icons for more appropriate ones if available
@@ -14,31 +15,26 @@ const navLinks = [
   {
     label: 'Token',
     link: '/token-info',
-    value: 'info',
     icon: <FiberSmartRecordOutlined />,
   },
   {
     label: 'Staking',
     link: '/staking',
-    value: 'staking',
     icon: <AutoGraphOutlined />,
   },
   {
     label: 'Home',
     link: '/',
-    value: '/',
     icon: <HomeOutlined />,
   },
   {
     label: 'Dashboard',
     link: '/dashboard',
-    value: 'dashboard',
     icon: <DashboardOutlined />,
   },
   {
     label: 'More',
     link: 'sidemenu',
-    value: 'sidemenu',
     icon: <MenuOutlined />,
   },
 ];
@@ -48,9 +44,11 @@ const root = { position: 'fixed', bottom: -1, width: '100vw' };
 
 const BottomNav = () => {
   const [value, setValue] = useState('/');
+  const router = useRouter();
 
   const handleChange = (_, newValue) => {
     setValue(newValue);
+    router.push(newValue);
   };
 
   return (
@@ -59,7 +57,7 @@ const BottomNav = () => {
         <BottomNavigationAction
           key={`${i}_${link.label}`}
           label={link.label}
-          value={link.value}
+          value={link.link}
           icon={link.icon}
         />
       ))}
